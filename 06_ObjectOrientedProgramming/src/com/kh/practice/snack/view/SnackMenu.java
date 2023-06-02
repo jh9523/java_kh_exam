6557037d686ec9e5c64fd342855ac73992e1aecb
jh9523 <jhjhnft@gmail.com>
2023-06-02 11:35:12 +0900

    KH 객체 실습 문제
    Run 클래스에서 동작함

---
+++ 06_ObjectOrientedProgramming/src/com/kh/practice/snack/view/SnackMenu.java
@@ -0,0 +1,34 @@
+package com.kh.practice.snack.view;
+
+import java.util.Scanner;
+import com.kh.practice.snack.controller.SnackController;
+
+public class SnackMenu {
+	private Scanner sc = new Scanner(System.in);
+	private SnackController scr = new SnackController();
+	
+	public void menu() {
+		//TODO
+		System.out.println("스낵류를 입력하세요.");
+		System.out.println("종류 : ");
+		String kind = sc.nextLine();
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		System.out.println("맛 : ");
+		String flavor = sc.nextLine();
+		System.out.println("개수 : ");
+		int numOf = sc.nextInt();
+		System.out.println("가격 : ");
+		int price = sc.nextInt();
+		sc.nextLine();		
+		System.out.println(scr.saveData(kind, name, flavor, numOf, price));
+		System.out.println("저장한 정보를 확인하시겠습니까?(y/n) : ");
+		char check = sc.nextLine().charAt(0);
+		if(check == 'y') {
+			System.out.println(scr.confirmData());
+		}
+		
+		
+	}
+
+	}
